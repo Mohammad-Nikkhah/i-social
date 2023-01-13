482f9c1404ed427d676e6f1e449d8353a0f702a4
Mohammad-Nikkhah <mohammadnikan47@gmail.com>
2023-01-13 22:43:05 +0330

    v2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,44 @@
-import { AppBar, Toolbar } from '@mui/material'
+import { AppBar, Avatar, Badge, InputBase, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { Box, styled } from '@mui/system';
+import { Mail, Pets ,NotificationAdd } from '@mui/icons-material';
+
+const StyledToolbar = styled(Toolbar)({
+  display:'flex',
+  justifyContent:"space-between",
+});
+
+const Search = styled('div')(( {theme}) => ({
+     backgroundColor:"white",
+     width:"40%",
+     padding:"0 10px",
+     borderRadius:theme.shape.borderRadius
+}));
+const Icons = styled(Box)(( {theme}) => ({
+     display:"flex",
+     alignItems:"center",
+     gap:"20px"
+}));
 
 const Navbar = () => {
   return (
     <AppBar position='stic'>
-      <Toolbar>
-          Navbar
-      </Toolbar>
+      <StyledToolbar>
+          <Typography variant='h6' sx={{display:{xs:"none",sm:"block"}}}>
+             I-social
+          </Typography>
+          <Pets sx={{display:{xs:"block",sm:"none"}}}/>
+          <Search><InputBase placeholder='search ...'/></Search>
+          <Icons>
+              <Badge badgeContent={4} color='error'>
+                  <Mail/>
+              </Badge>
+              <Badge badgeContent={2} color='error'>
+                 <NotificationAdd/>
+              </Badge>
+              <Avatar sx={{width:"30px",height:"30px"}} src='https://avatars.githubusercontent.com/u/78091800?v=4'></Avatar>
+          </Icons>
+      </StyledToolbar>
     </AppBar>
   )
 }
