9a509a766e648a46c24379ac3aa1b779f3ccc8a6
Mohammad-Nikkhah <mohammadnikan47@gmail.com>
2023-01-13 23:15:06 +0330

    Finish Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { AppBar, Avatar, Badge, InputBase, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Avatar, Badge, InputBase, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import { Box, styled } from '@mui/system';
 import { Mail, Pets ,NotificationAdd } from '@mui/icons-material';
 
@@ -15,12 +15,25 @@ const Search = styled('div')(( {theme}) => ({
      borderRadius:theme.shape.borderRadius
 }));
 const Icons = styled(Box)(( {theme}) => ({
-     display:"flex",
+     display:"none",
      alignItems:"center",
-     gap:"20px"
+     gap:"20px",
+     [theme.breakpoints.up('sm')]:{
+        display:"flex"
+     }
+}));
+const UserBox = styled(Box)(( {theme}) => ({
+  display:"flex",
+  alignItems:"center",
+  gap:"10px",
+  [theme.breakpoints.up('sm')]:{
+    display:"none"
+ }
 }));
 
+
 const Navbar = () => {
+  const [open,setOpen] = useState(false)
   return (
     <AppBar position='stic'>
       <StyledToolbar>
@@ -36,9 +49,31 @@ const Navbar = () => {
               <Badge badgeContent={2} color='error'>
                  <NotificationAdd/>
               </Badge>
-              <Avatar sx={{width:"30px",height:"30px"}} src='https://avatars.githubusercontent.com/u/78091800?v=4'></Avatar>
+              <Avatar sx={{width:"30px",height:"30px"}} onClick={e=>setOpen(true)} src='https://avatars.githubusercontent.com/u/78091800?v=4'></Avatar>
           </Icons>
+          <UserBox onClick={e=>setOpen(true)}>
+              <Typography variant='h6'>Moniik</Typography>
+              <Avatar sx={{width:"30px",height:"30px"}}  src='https://avatars.githubusercontent.com/u/78091800?v=4'></Avatar>
+          </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={e=>setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
     </AppBar>
   )
 }
